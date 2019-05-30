43b2cee9cd7107e9e2e139f2a2e4221a45cc7d42
fatemugun <fadefatemugun@outlook.com>
2019-05-30 17:00:26 +0800

    子集AC

--- src/recallAlgorithm/SubSet.java
+++ src/recallAlgorithm/SubSet.java
@@ -2,7 +2,7 @@ package recallAlgorithm;
 import java.util.*;
 //https://leetcode-cn.com/problems/subsets/
 public class SubSet {
-    public List<List<Integer>> subsets(int[] nums) {
+    public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res=new ArrayList<List<Integer>>();
         List<Integer> temp=new ArrayList<Integer>();
         res.add(temp);
@@ -11,13 +11,21 @@ public class SubSet {
         countSubSet(res,temp,nums,0);
         return res;
     }
-    public void countSubSet(List<List<Integer>> res,List<Integer> temp,int[] nums,int index) {
+    public static void countSubSet(List<List<Integer>> res,List<Integer> temp,int[] nums,int index) {
     	if(res.size()==Math.pow(2, nums.length))
     		return;
-    	temp.add(nums[index]);
-    	if(!res.contains(temp)) {
-    		
+    	for(int i=index;i<nums.length;i++) {
+    		temp.add(nums[i]);
+    		res.add(new ArrayList<Integer>(temp));
+    		countSubSet(res,temp,nums,i+1);
+    		temp.remove(temp.size()-1);
     	}
     	
+    }
+    public static void main(String[] args) {
+    	int[] nus= {1,2,3};
+    	List<List<Integer>>res;
+    	res=subsets(nus);
+    	System.out.println("");
     }
 }
